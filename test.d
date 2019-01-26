6415e0f9d937187143b4d4bd3c9589b81bed2fe9
Victor Porton <porton@narod.ru>
2019-01-26 17:34:13 +0200

    added tests (passed)

--- test.d
+++ test.d
@@ -1,8 +1,33 @@
 module test;
 
 import std.stdio;
+import libcomcom_wrapper;
 
 void main(string[] args)
 {
+    // Tests
+    libComComInitializer();
+    scope(exit) libComComDestructor();
+
+    assert(runCommand("cat", ["cat"], "qwe", 5000) == "qwe");
+
+    {
+        char[] buf = new char[1000000];
+        for(int i=0; i<buf.sizeof; ++i)
+            buf[i] = (cast(char)i) % 3;
+        assert(runCommand("cat", ["cat"], cast(string) buf, 5000) == buf);
+    }
+
+    {
+        char[] buf = new char[1000000];
+        for(int i=0; i<buf.sizeof; ++i)
+            buf[i] = (cast(char)i) % 3;
+        string output = runCommand("dd",
+                                   ["dd", "bs=100000", "count=10", "iflag=fullblock"],
+                                   cast(string) buf,
+                                   5000);
+        assert(output == buf);
+    }
+
     writeln("Tests passed.");
 }
